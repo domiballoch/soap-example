fa9ae90ac21d40dbf8e629f1446904f5293bc979
Dominic Balloch <bigdom76@gmail.com>
2021-12-07 23:38:32 +0000

    Updating custom exception - typo

--- src/main/java/dom/soapexample/exception/BookNotFoundException.java
+++ src/main/java/dom/soapexample/exception/BookNotFoundException.java
@@ -3,7 +3,7 @@ package dom.soapexample.exception;
 import org.springframework.ws.soap.server.endpoint.annotation.FaultCode;
 import org.springframework.ws.soap.server.endpoint.annotation.SoapFault;
 
-@SoapFault(faultCode = FaultCode.CUSTOM, customFaultCode = "{http://www.soap.com/jaxb}001_COURSE_NOT_FOUND")
+@SoapFault(faultCode = FaultCode.CUSTOM, customFaultCode = "{http://www.soap.com/jaxb}001_BOOK_NOT_FOUND")
 public class BookNotFoundException extends RuntimeException {
 
     private static final long serialVersionUID = 3518170101751491969L;
