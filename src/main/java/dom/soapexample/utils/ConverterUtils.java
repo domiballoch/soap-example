9f32f16b086d84d284d1b058fbba12203f58ec38
Dominic Balloch <bigdom76@gmail.com>
2022-03-15 09:24:17 +0000

    Adding tests and clean up

--- src/main/java/dom/soapexample/utils/ConverterUtils.java
+++ src/main/java/dom/soapexample/utils/ConverterUtils.java
@@ -1,16 +1,12 @@
 package dom.soapexample.utils;
 
-import com.soap.jaxb.Book;
-import com.soap.jaxb.ObjectFactory;
 import dom.soapexample.config.JaxbConfig;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
 
 import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBElement;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
@@ -18,88 +14,16 @@ import javax.xml.soap.MessageFactory;
 import javax.xml.soap.SOAPException;
 import javax.xml.soap.SOAPMessage;
 import java.io.ByteArrayInputStream;
-import java.io.File;
 import java.io.IOException;
-import java.io.StringReader;
 import java.io.StringWriter;
 
 @Slf4j
 @Component
-//@UtilityClass
 public class ConverterUtils extends ApplicationContextUtils {
-
-    //Simple way - one type
-    public static StringReader convertXMLToBook(String xml) throws JAXBException {
-        log.info("Unmarshalling Book to Object: ", xml);
-        JAXBContext jaxbContext = JAXBContext.newInstance(Book.class);
-        Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
-
-        StringReader stringReader = new StringReader(xml);
-        unmarshaller.unmarshal(stringReader);
-        log.info("Unmarshalled XML as Book: {}", stringReader);
-        return stringReader;
-    }
-
-    //Simple way - one type
-    public static String convertBookToXML(Book book) throws JAXBException {
-        log.info("Marshalling Book to XML: {}", book);
-        StringWriter stringWriter = new StringWriter();
-        JAXBContext jaxbContext = JAXBContext.newInstance(book.getClass());
-        Marshaller marshaller = jaxbContext.createMarshaller();
-        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-
-        marshaller.marshal(book, stringWriter);
-        log.info("Marshalled Book as String: {}", stringWriter.toString());
-        return stringWriter.toString();
-    }
-
-    // ----- GENERIC WAY -----//
-
-    /**
-     * Unmarshal XML to Object - Generic
-     * Can unmarshal to object class, stringReader, byteArrayOutputStream etc
-     *
-     * @param clazz
-     * @param xml
-     * @param <T>
-     * @return
-     * @throws JAXBException
-     */
-    public static <T> T convertXMLToObject(Class<T> clazz, String xml) throws JAXBException {
-        log.info("Unmarshalling XML to Object: ", xml);
-        JAXBContext jaxbContext = JAXBContext.newInstance(clazz);
-        Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
-
-        StringReader stringReader = new StringReader(xml);
-        unmarshaller.unmarshal(stringReader);
-        log.info("Unmarshalled XML as Object: {}", stringReader);
-        return (T) stringReader;
-    }
-
-    /**
-     * Marshal Object to XML - Generic
-     * Can marshal to file, stringWriter or system.out etc
-     *
-     * @param object
-     * @param <T>
-     * @return
-     * @throws JAXBException
-     */
-    public static <T> String convertObjectToXML(T object) throws JAXBException {
-        log.info("Marshalling Object to XML: {}", object);
-        StringWriter stringWriter = new StringWriter();
-        JAXBContext jaxbContext = JAXBContext.newInstance(object.getClass());
-        Marshaller marshaller = jaxbContext.createMarshaller();
-        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-
-        marshaller.marshal(object, stringWriter);
-        log.info("Marshalled Object as String: {}", stringWriter.toString());
-        return stringWriter.toString();
-    }
 
     //----- METHODS USING JAXB CONTEXT BEAN -----//
 
-    @Qualifier("book")
+    @Qualifier("book") //Using qualifier
     @Autowired
     private JAXBContext bookJaxbContext;
 
---
+++ src/test/java/dom/soapexample/service/BookServiceTest.java
@@ -0,0 +1,41 @@
+package dom.soapexample.service;
+
+import dom.soapexample.model.Book;
+import dom.soapexample.repository.BookRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+public class BookServiceTest {
+
+    @Autowired
+    private BookServiceImpl bookService;
+
+    @Test
+    public void shouldFindOneBookByTitle() {
+        final Book result = bookService.findBook("Frankenstein");
+        assertThat(result.getTitle()).isEqualTo("Frankenstein");
+        assertThat(result.getAuthor()).isEqualTo("Mary Shelley");
+        assertThat(result.getCategory()).isEqualTo(com.soap.jaxb.Category.HORROR);
+        assertThat(result.getPrice()).isEqualTo(new BigDecimal("5.39"));
+        assertThat(result.getStock()).isEqualTo(100);
+    }
+
+    @Test
+    public void shouldFindAllBooksBy() {
+        final List<Book> result = bookService.findAllBooks();
+        assertThat(result.size()).isEqualTo(12);
+    }
+
+    @Test
+    public void shouldDeleteOneBook() {
+        bookService.deleteBookByIsbn(1L);
+        assertThat(BookRepository.books.get(1)).isNull();
+    }
+}
